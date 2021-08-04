a8573aabcdb967e2707d30996c5ba48081467154
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2021-08-04 22:48:53 +0200

    Fike to merge the files from the condor processing

---
+++ merge_coffea.py
@@ -0,0 +1,49 @@
+# This code is used to merge the coffea files that are produced from a condor processing
+from coffea.util import save, load
+from tqdm import tqdm
+import argparse
+import os
+
+# For argument parsing
+parser = argparse.ArgumentParser(description="Merge coffea files from condor processing")
+parser.add_argument("-d", "--data", help="Merge the data files", action="store_true")
+parser.add_argument("-p", "--hist", help="Merge the hist files", action="store_true")
+args = parser.parse_args()
+
+# Function to create a list with all coffea files in a path
+path = 'output/Charmonium_2017_Nano_Coffea/'
+name = 'Charmonium_2017'
+
+def merg_files(path, name):
+
+    if args.data:
+        typ = '.coffea'
+        sv = 'data'
+    elif args.hist:
+        typ = 'hists.coffea'
+        sv = 'hists'
+    else:
+        print("You should provide an option: -d for merge data files or -h to merge the hist files")
+
+    files = []
+    with os.scandir(path) as aux:
+        for file in aux:
+            if file.name.endswith(typ) and (file.stat().st_size != 0):
+                files.append(file.path)
+    # Takes the first to start the accumulator
+    acc = load(files[0])
+    # Take the length of the list
+    le_files = len(files) # int(round(len(files), 0))
+
+    # For accumulate the files
+    for i in tqdm(range(1, le_files), desc="Processing", unit="files"):
+        acc += load(files[i])
+    save(acc, sv + '_' + name + '.coffea')
+    print ("Finished")
+
+merg_files(path, name)
+
+
+ 
+
+
